889ee4f501202d241a10d27b36c616a7e82951c3
Demyan <serg180992@gmail.com>
2023-03-29 22:02:06 +0300

    изменили 2 файла

--- python3 J143.py
+++ python3 J143.py
@@ -6,25 +6,25 @@ bot = telebot.TeleBot('6277583836:AAFxW5ZBzKEpWqoMANc19kFNHpXdiCZQMy0')
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Поздороваться")
+    btn1 = types.KeyboardButton("👋Поздороваться")
     markup.add(btn1)
-    bot.send_message(message.from_user.id, "Привет! Я твой бот-поздравитель!", reply_markup=markup)
+    bot.send_message(message.from_user.id, "👋Привет! Я твой бот-поздравитель!", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'Поздороваться':
+    if message.text == '👋Поздороваться':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Получить поздравление с годовщиной')
-        btn2 = types.KeyboardButton('Просто комплимент')
-        btn3 = types.KeyboardButton('Признание в любви')
+        btn1 = types.KeyboardButton('❓Получить поздравление с годовщиной')
+        btn2 = types.KeyboardButton('❓Просто комплимент')
+        btn3 = types.KeyboardButton('❓Признание в любви')
         markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, 'Задай интересующий тебя вопрос', reply_markup=markup) #ответ бота
+        bot.send_message(message.from_user.id, '❓Задай интересующий тебя вопрос', reply_markup=markup) #ответ бота
 
-    elif message.text == 'Получить поздравление с годовщиной':
-        bot.send_message(message.from_user.id, 'Я поздравляю тебя с нашей шестой годовщиной свадьбы! Пусть у нас не всегда всё хорошо, но я счаслив быть с тобой!', parse_mode='Markdown')
-    elif message.text == 'Просто комплимент':
-        bot.send_message(message.from_user.id, 'Ты очень красивая!', parse_mode='Markdown')
-    elif message.text == 'Признание в любви':
-        bot.send_message(message.from_user.id, 'Я тебя люблю несмотря ни на что!', parse_mode='Markdown')
+    elif message.text == '❓Получить поздравление с годовщиной':
+        bot.send_message(message.from_user.id, '🎊Я поздравляю тебя с нашей шестой годовщиной свадьбы! Пусть у нас не всегда всё хорошо, но я счаслив быть с тобой!', parse_mode='Markdown')
+    elif message.text == '❓Просто комплимент':
+        bot.send_message(message.from_user.id, '🫦Ты очень красивая!', parse_mode='Markdown')
+    elif message.text == '❓Признание в любви':
+        bot.send_message(message.from_user.id, '❤Я тебя люблю несмотря ни на что!', parse_mode='Markdown')
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
